4af62db3621c8d321a04edfba65c6de642cb2d34
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2019-10-22 11:59:39 +0530

    Revert "working"
    This reverts commit 373b60ee559556957882379b6bcc22ced0a64a5e.

--- server.js
+++ server.js
@@ -2,16 +2,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const fs = require('fs');
 const bodyparser = require('body-parser');
 const compression = require('compression');
 const helmet = require('helmet');
 const TelegramBot = require('node-telegram-bot-api');
-let Message = '';
 
 const app = express();
 const port = process.env.PORT;
 const token = process.env.token;
 const url = "https://telegram----bot.herokuapp.com/";
+let Messages = "";
 
 app.use(bodyparser.json());
 app.use(morgan('combined'));
@@ -24,25 +25,26 @@ const bot = new TelegramBot(token);
 bot.setWebHook(`${url}/bot${token}`);
 // We are receiving updates at the route below!
 app.post(`/bot${token}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
 
 
-app.listen(port, () => {
-  console.log(`Our site is hosted on ${port}! If you donot know to open just go to browser and type (localhost:${port})`)
-});
+app.listen(port, () => { console.log(`Our site is hosted on ${port}! If you donot know to open just go to browser and type (localhost:${port})`)});
 
 bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, 'I am alive!' + msg.chat.id + ` \n\n ${msg.message_id}`);
-  console.log("\n" + JSON.stringify(msg));
-  if (msg.text.toLowerCase === 'delete') {
-    Message = ""
-  } else if (msg.text) {
-    Message = msg.text;
-  }
-});
+    bot.sendMessage(msg.chat.id, 'I am alive!'+msg.chat.id+` \n\n ${msg.message_id}`);
+    console.log("\n"+JSON.stringify(msg));
 
-app.get('/message', (req, res) => {
-  res.send(Message);
-});
+    if(msg.text !== 'delete'){
+    Messages = msg.text;
+    } else if (msg.text === 'delete') {Messages = ""}
+  });
+
+  app.get('/', (req, res) => {
+    if(Messages.file_id) {
+      res.sendFile(Messages);
+    } else {
+    res.send(Messages);
+    }
+  })
