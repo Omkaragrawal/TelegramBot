76a7dafbc70b1000b87ab7a84afb95471f6c5e48
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2019-10-22 12:00:23 +0530

    Revert "tried to solve"
    This reverts commit 6b6cc79e6fcf6b0dacb58c4ad3632d89e403da36.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const bodyparser = require('body-parser');
 const compression = require('compression');
 const helmet = require('helmet');
 const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT;
